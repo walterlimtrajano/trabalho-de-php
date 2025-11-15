46218d5f72f4141008c044da3199499878813205
Gabriel Rabelo Barbosa <gaabrielrabelo@gmail.com>
2025-11-15 15:59:05 -0300

    .

--- ecommerce/includes/header.php
+++ ecommerce/includes/header.php
@@ -7,22 +7,24 @@
     <link rel="stylesheet" href="/trabalho-de-php/ecommerce/assets/css/style.css">
 </head>
 <body>
-    <header class="site-header">
-        <div class="header-inner">
-            <div class="brand">
-                <h1>🛍️ E-commerce</h1>
-            </div>
+<header class="site-header">
 
-            <nav class="main-nav">
-                <a href="/trabalho-de-php/ecommerce/index.php">Início</a> |
-                <a href="/trabalho-de-php/ecommerce/produtos/listar.php">Produtos</a> |
-                <a href="/trabalho-de-php/ecommerce/carrinho/visualizar.php">Carrinho</a>
-            </nav>
+    <div class="header-inner">
 
-            <div class="auth">
-                <a href="/trabalho-de-php/ecommerce/login.php" class="btn auth-btn">Entrar / Cadastrar</a>
-            </div>
+        <div class="brand">
+            <h1>🛍️ E-commerce</h1>
         </div>
 
-        <hr>
-    </header>
+        <nav class="main-nav">
+            <a href="/trabalho-de-php/ecommerce/index.php">Início</a>
+            <a href="/trabalho-de-php/ecommerce/produtos/listar.php">Produtos</a>
+            <a href="/trabalho-de-php/ecommerce/carrinho/visualizar.php">Carrinho</a>
+        </nav>
+
+        <div class="auth">
+            <a href="/trabalho-de-php/ecommerce/login.php" class="btn auth-btn">Entrar / Cadastrar</a>
+        </div>
+
+    </div>
+    <hr>
+</header>
